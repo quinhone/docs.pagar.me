f4f692908f58d74003cf856da6d740ee2df96a66
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2014-11-06 16:35:05 -0200

    Add support for setting the title's id on markdown.

--- docs/lib/redcarpet_header_fix.rb
+++ docs/lib/redcarpet_header_fix.rb
@@ -1,6 +1,12 @@
 module RedcarpetHeaderFix
   def header(text, level, id)
     clean_id = id.gsub(/[\.]/, '-').gsub(/[^a-zA-Z0-9\-_]/, '')
+
+	/{#(.*)}/.match text do |m|
+	  text.gsub! m[0], ''
+	  clean_id = m[1]
+	end
+
     "<h#{level} id='#{clean_id}'>#{text}</h1>"
   end
 end
